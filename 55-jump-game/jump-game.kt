6091d2632969a4d802be8f60119309bb2b51502e
eongiin <90626691+eongiin@users.noreply.github.com>
2024-04-12 17:25:10 +0900

    Time: 253 ms (98.16%) | Memory: 39.5 MB (86.52%) - LeetSync

---
+++ 55-jump-game/jump-game.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun canJump(nums: IntArray): Boolean {
+        var targetIndex = nums.size - 1
+        for(i in nums.size - 2 downTo 0) {
+            if(i + nums[i] >= targetIndex) targetIndex = i
+        }
+       return targetIndex == 0 
+    }
+}
